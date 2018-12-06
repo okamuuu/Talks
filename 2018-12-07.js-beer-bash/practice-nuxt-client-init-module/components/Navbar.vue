bd6ee55e2bf825a07b0ea91e572b3fba065653cf
okamuuu <okamuuu@gmail.com>
2018-12-06 21:59:36 +0900

    add one page

---
+++ 2018-12-07.js-beer-bash/practice-nuxt-client-init-module/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar container" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
+  
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start"></div>
+      <div class="navbar-end">
+      
+        <nuxt-link class="navbar-item is-size-6" to="/one">One</nuxt-link>
+        <nuxt-link class="navbar-item is-size-6" to="/tow">Two</nuxt-link>
+        <nuxt-link class="navbar-item is-size-6" to="/three">Three</nuxt-link>
+  
+      </div>
+    </div>
+  </nav>
+</template>
