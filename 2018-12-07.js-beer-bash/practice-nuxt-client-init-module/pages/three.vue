df00f0dfe6f5f85367e618f8388b5aab163eadf2
okamuuu <okamuuu@gmail.com>
2018-12-08 09:50:58 +0900

    add doc

---
+++ 2018-12-07.js-beer-bash/practice-nuxt-client-init-module/pages/three.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="one-container" style="display:flex;flex-direction:column;justify-content:center;">
+    <h1 class="title" v-if="isClient()">Three Client</h1>
+    <h1 class="title" v-else>Three Server</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  methods: {
+    isClient() {
+      return (typeof window !== 'undefined' && window.document)
+    }
+  }
+}
+</script>
+
+<style>
+.one-container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 180px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+</style>
+
